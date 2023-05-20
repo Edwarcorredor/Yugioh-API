5fd7ea2992f99e291790737e9d47d93de3aa868c
EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-05-20 00:14:10 -0500

    feat: :zap: Arreglo del frontend
    Se crea el primero diseño, dividiendo el ingreso de datos y la parte donde se mostrará el top de cartas

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,7 @@
 import { ElementCard } from "/components/element-card.js";
+const cardsTop = document.querySelector("#cards-top");
 
+for(let i = 0; i < 5; i++) {
 const elementCard = new ElementCard()
-
-document.body.appendChild(elementCard)
-
-
-const elementCard2 = new ElementCard()
-document.body.appendChild(elementCard2)
+cardsTop.appendChild(elementCard)
+}
